ff7a6105e962c82024c46d1f14f47f231db36cd4
Philipp Mangelow <pmangelow@googlemail.com>
2012-07-08 16:05:04 +0200

    Clean

--- src/de/mangelow/network/RadioNetwork.java
+++ src/de/mangelow/network/RadioNetwork.java
@@ -16,35 +16,31 @@ package de.mangelow.network;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
 public class RadioNetwork extends Activity {
-	 private TelephonyManager tm;
-	 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS) {
-			Toast.makeText(getBaseContext(), "UMTS", Toast.LENGTH_LONG).show();
-        }
-        if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_EDGE) {
-			Toast.makeText(getBaseContext(), "EDGE", Toast.LENGTH_LONG).show();
-        }
-        if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_GPRS) {
-			Toast.makeText(getBaseContext(), "GPRS", Toast.LENGTH_LONG).show();
-        }
-        if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_UNKNOWN) {
-			Toast.makeText(getBaseContext(), "UNKNOWN", Toast.LENGTH_LONG).show();
-        }
 
+	private Context context;	
+	private TelephonyManager tm;
 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		context = getApplicationContext();
+
+		// Toast current Network Type
+		tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		String type = "UNKOWN";
+		if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS)type = "UMTS";
+		if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_EDGE)type = "EDGE";
+		if (tm.getNetworkType() == TelephonyManager.NETWORK_TYPE_GPRS)type = "GPRS";
+		Toast.makeText(context, type, Toast.LENGTH_LONG).show();
+
+		// Launch Activity RadioInfo
 		Intent i = new Intent();
 		i.setClassName( "com.android.settings", "com.android.settings.RadioInfo" );
 		startActivity(i);
-		finish();
-    }
+	}
 }
